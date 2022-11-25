5d11b9faeafd8c26800e984d118e0b2ff3fe7a13
“fernandorenan” <nuncavouteabandonar@gmail.com>
2022-11-24 21:28:27 -0300

    Add helper birth notification

--- app/helpers/cadastros_helper.rb
+++ app/helpers/cadastros_helper.rb
@@ -1,2 +1,14 @@
 module CadastrosHelper
+
+    def birthday_notification
+        births = ""
+        @cadastros.each do |cadastro| 
+            if cadastro.birth.month == Date.today.month
+             births << "<li>Esse mês temos o aniversário de #{cadastro.name} dia #{cadastro.birth.day}. 
+                #{cadastro.birth == Date.today ? "<strong>É hoje!</strong>":"" }</li>"
+            end
+        end
+        %Q{Estamos no mês #{Date.today.month}: <ul>#{births}</ul></div>}.html_safe
+    end
+ 
 end
