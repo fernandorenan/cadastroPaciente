5b778863fe84d20e253f201c27f5ffe7b5c3629d
FernandoRenan <fernando.renan@locaweb.com.br>
2022-12-07 16:06:45 -0300

    Ajusta status

--- app/models/cadastro.rb
+++ app/models/cadastro.rb
@@ -23,7 +23,7 @@ class Cadastro < ApplicationRecord
   validates :name, :start_date, presence: true
 
 
-  scope :search_by_status, -> (status) { where("status = ?", status) if @cadastros.present? }
-  scope :search_by_name, -> (name) { where("name LIKE ?", "%#{name}%") if @cadastros.present? }
+  scope :search_by_status, -> (status) { where("status = ?", status) if status.present? }
+  scope :search_by_name, -> (name) { where("name LIKE ?", "%#{name}%") if name.present? }
   #Cadastro.where("name LIKE ?", "%#{'Fernando'}%")
 end
